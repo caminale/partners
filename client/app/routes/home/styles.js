748f5cd0688b38ee70b59eda6b031976df44b6a3
chazzfit <basson_j@esme.fr>
2017-03-11 15:30:47 +0100

    rien de new

---
+++ client/app/routes/home/styles.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet
+} from 'react-native';
+
+const styles = StyleSheet.create ({
+  container: {
+    marginTop: 100,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  texte: {
+    paddingVertical: 15,
+    color: 'black',
+    justifyContent: 'center',
+    fontSize: 30
+  },
+  button: {
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'black'
+  }
+});
+
+
+export default styles;
