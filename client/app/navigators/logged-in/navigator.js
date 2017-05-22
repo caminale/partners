612d8df17095bfd25b422d926f71253c0058e76d
chazzfit <basson_j@esme.fr>
2017-05-22 19:13:52 +0200

    fixed back button in principal navigator

--- client/app/navigators/logged-in/navigator.js
+++ client/app/navigators/logged-in/navigator.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
+import ReactNative from 'react-native';
 
 import {ConversationNavigator, MatchingNavigator, ProfileNavigator} from '../../navigators';
 
 import {TabBar} from '../../components';
+
+const {
+  BackAndroid
+} = ReactNative;
 
 class MyNavigator extends React.Component {                 //NAVIGATEUR de gestion des vues après connexion (LOGGED-IN)
   renderTabBar = () => {
     return <TabBar/>;                       //Appel du composant TabBar gérant l'accès aux différentes vues
   };
   render() {
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+
+      BackAndroid.exitApp();
+
+    });
     return (
       <ScrollableTabView                       //Bar gérant l'accès au différentes vues, passage de différentds paramètres et de l'accès
         tabBarPosition="bottom"               //a la fonction renderTabBar
